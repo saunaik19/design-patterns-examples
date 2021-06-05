ba4295effd1589c584b4ac0787ee32c1fb981548
saunaik19 <saunaik19@gmail.com>
2021-06-06 03:44:29 +0530

    Initial commit

---
+++ src/main/java/com/saurabh/example/designpatterns/DesignPatternsExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.example.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/example/designpatterns/DesignPatternsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.example.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
